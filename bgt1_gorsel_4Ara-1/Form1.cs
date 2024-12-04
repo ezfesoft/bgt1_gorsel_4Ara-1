acda1b916fe973828ca0d0e1d1a03ec52124a8c3
slymn_LAB2 <ezfesoftware@gmail.com>
2024-12-04 09:16:43 +0300

    Proje dosyası ekle.

---
+++ bgt1_gorsel_4Ara-1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bgt1_gorsel_4Ara_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int yas;
+            yas = Convert.ToInt32(textBox2.Text);
+            double sayi;
+            sayi = Convert.ToDouble(textBox1.Text);
+            double sonuc;
+            if (yas <= 12)
+            {
+                sonuc = sayi * 0.5;
+            }
+            else if (yas <= 24)
+            {
+                sonuc = sayi * 0.9;
+            }
+            else if (yas >= 65)
+            {
+                sonuc = sayi * 0.7;
+            }
+            else
+            {
+                sonuc = sayi;
+            }
+            label5.Text = Convert.ToString(sonuc);
+
+
+
+
+        }
+    }
+}
